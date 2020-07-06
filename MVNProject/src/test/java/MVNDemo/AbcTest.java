c6f4689abb5883297c4b492c57b7f2308a182b52
chaitanyavengampally <chaitanyavengampally@gmail.com>
2020-07-06 13:22:42 +0530

    first sample code for test

---
+++ MVNProject/src/test/java/MVNDemo/AbcTest.java
@@ -0,0 +1,20 @@
+package MVNDemo;
+
+import org.testng.annotations.Test;
+
+public class AbcTest {
+	
+	@Test
+	public void mvnAbc() {
+		
+		System.out.println("Welocome to Maven");
+	}
+	
+
+	@Test
+	public void m2() {
+		
+		System.out.println("welcome to maven method2.......");
+		
+	}
+}
